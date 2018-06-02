774677e3276196a118afce256e0b0adc78773f8d
bomianmian <bomianmian@qq.com>
2018-06-02 13:00:59 +0800

    2

--- portfollo/settings.py
+++ portfollo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'portfollo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
